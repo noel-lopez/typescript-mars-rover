2872402365c56eb142a2c90e5300d90c06883c0a
noel-lopez <nlopez.publi@gmail.com>
2024-02-19 12:16:49 +0100

    feature: should face South when give "LL" command line from initial position

--- test/rover.test.ts
+++ test/rover.test.ts
@@ -1,6 +1,9 @@
 import { describe, expect, test } from 'vitest'
 class Rover {
   public execute(commands: string) {
+    if (commands === "LL") {
+      return '0:0:S'
+    }
     return '0:0:W'
   }
 }
@@ -9,5 +12,9 @@ describe('rover', () => {
   test('should face West when given a single L command from initial position', () => {
     const rover = new Rover()
     expect(rover.execute('L')).toBe('0:0:W')
+  })
+  test('should face South when give "LL" command line from initial position', () => {
+    const rover = new Rover()
+    expect(rover.execute('LL')).toBe('0:0:S')
   })
 })
